194e1e4376b21bc0d8a583fd4e191a1b532627e2
alanleew <alanleew412@gmail.com>
2025-07-31 16:49:58 -0700

    Created decorator functions: make_bold(), make_emphasis(), make_underlined().
    Ran into a problem where they would not work without using wrapper function within decorator function.

--- hello.py
+++ hello.py
@@ -3,6 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 # print(__name__)
 
+def make_bold(func):
+    def wrapper():
+        return "<b>"+func()+"</b"
+    return wrapper
+
+def make_emphasis(func):
+    def wrapper():
+        return "<em>"+func()+"</em>"
+    return wrapper
+
+def make_underlined(func):
+    def wrapper():
+        return "<u>"+func()+"</u"
+    return wrapper
+
 @app.route('/')
 def hello_world():
     return ('<h1 style="text-align: center">Hello, World!</h1>'
@@ -11,12 +26,18 @@ def hello_world():
             '<img src="https://pa1.aminoapps.com/6241/7c70e19703679f9a46690c1349f5a1e2324a29ee_hq.gif" width=400>')
 
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
 def bye():
     return 'Bye!'
 
-@app.route('/username/<name>/<int:number>')  # <> becomes a variable
-def greet(name, number):
-    return f"Hello {name}! You are {number} years old!"
+# Uses Variable Rules from Flask Doc
+# <number> uses converter to type cast str -> int
+@app.route('/username/<name_variable>/<int:number>')
+def greet(name_variable, number):
+    return f"Hello {name_variable}! You are {number} years old!"
 
 if __name__ == "__main__":
     app.run(debug=True)
+# Turns Debugger On!
